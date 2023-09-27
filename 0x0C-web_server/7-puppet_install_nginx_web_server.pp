aa3c2cb108ea4052caca2b115ae2f7b6c6667a00
sylvieshimwauwase <slvshimwa@gmail.com>
2023-09-27 14:51:11 +0200

    Web Server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# Puppet script to install nginx
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+exec {'Hello World':
+  command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
