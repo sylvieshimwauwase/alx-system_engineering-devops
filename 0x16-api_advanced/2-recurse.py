86062a6967f953704819be535210c572ba600245
sylvieshimwauwase <slvshimwa@gmail.com>
2023-11-07 14:12:49 +0200

    API Advanced

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""returning a list containing titles of all articles"""
+
+from requests import get
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+    """to ten post recursively"""
+
+    global after
+    headers = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {'after': after}
+
+    results = get(url, headers=headers, params=params, allow_redurects=False)
+
+    if results.status_code == 200:
+        afterData = results.json().get("data").get("after")
+
+        if afterData is not None:
+            after = afterData
+            recurse(subreddit, hot_list)
+
+        titles = results.json().get("data").get("children")
+        for title_ in titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
+    else:
+        return (None)
